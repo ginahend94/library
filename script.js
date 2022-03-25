951ce5622e49610adc58d037ea6c04cdebb9936a
Gina Henderson <gina@ginahenderson.me>
2022-03-24 23:52:46 -0500

    Style modal

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+// MODAL
+const wrapper = document.querySelector('.wrapper');
+const modal = document.querySelector('.modal');
+const modalBg = document.querySelector('.modal-bg');
+const form = document.querySelector('form');
+const formCancel = form.querySelector('a');
+
+formCancel.addEventListener('click', () => modal.style.display = 'none');
+wrapper.querySelector('button').addEventListener('click', () => modal.style.display = 'flex');
+
+modalBg.addEventListener('click', e => {
+    modal.style.display = 'none';
+})
+
+
 const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
 const pagesInput = document.getElementById('pages');
 const readInput = document.getElementById('read');
 const table = document.querySelector('table');
+
+
 
 document.querySelector("form").addEventListener("submit", (e) => {
 	e.preventDefault();
